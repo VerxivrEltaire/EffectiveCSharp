c42930165b2acc9efc70c0f57e8fcc217a291233
Kelechi <kenneth.iheme@gmail.com>
2024-06-20 21:15:26 -0400

    Project Commit

---
+++ Chapter 02 Communicating in C Sharp/Program.cs
@@ -0,0 +1,18 @@
+﻿
+// STATEMENTS 
+
+int x = 5; 
+x = x + 2; 
+Console.WriteLine(x); 
+
+// COMMENTS
+
+// This is a single-line comment
+
+/*
+This is a
+multi-line comment
+*/
+
+int y = 10; // This comment explains that y is assigned the value 10
+
